03ef196b3df969675f75076a1399afb0f01b70e9
srhee34 <60160057+joshrhee@users.noreply.github.com>
2023-09-10 20:32:52 -0400

    Add Experience section

---
+++ src/components/experience.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import {
+    VerticalTimeline,
+    VerticalTimelineElement
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
+import { experiencesData } from "@/lib/data";
+
+export default function Experience() {
+    return (
+        <section id="experience" className="mb-28">
+            <SectionHeading>My Experience</SectionHeading>
+            <VerticalTimeline lineColor="">
+                {experiencesData.map((item, index) => (
+                    <React.Fragment key={index}>
+                        <VerticalTimelineElement
+                            contentStyle={{
+                                background: "#f3f4f6",
+                                boxShadow: "none",
+                                border: "1px solid rgba(0, 0, 0, 0.05)",
+                                textAlign: "left",
+                                padding: "1.3rem 2rem"
+                            }}
+                            contentArrowStyle={{
+                                borderRight: "0.4rem solid #9ca3af"
+                            }}
+                            date={item.date}
+                            icon={
+                                <div className="flex justify-center items-center w-full h-full">
+                                    <img
+                                        src={item.image.src}
+                                        alt={item.title}
+                                        className="w-[60%] h-[60%] object-contain"
+                                    />
+                                </div>
+                            }
+                            iconStyle={{
+                                background: "white",
+                                fontSize: "1.5rem"
+                            }}
+                        >
+                            <h3 className="font-bold capitalize">
+                                {item.title}
+                            </h3>
+                            <p className="font-semibold !mt-2 text-gray-700">
+                                {item.company}
+                            </p>
+
+                            <ul className="mt-5 list-disc ml-5 space-y-2">
+                                {item.points.map((point, index) => (
+                                    <li className="!mt-1 !font-normal text-gray-700">
+                                        {point}
+                                    </li>
+                                ))}
+                            </ul>
+                        </VerticalTimelineElement>
+                    </React.Fragment>
+                ))}
+            </VerticalTimeline>
+        </section>
+    );
+}
--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,11 +1,12 @@
 import React from "react";
-import { CgWorkAlt } from "react-icons/cg";
-import { FaReact } from "react-icons/fa";
-import { LuGraduationCap } from "react-icons/lu";
 
 import visionariez from "../../public/visionariez.png";
 import creditCardTrackerGenerator from "../../public/credit-card-tracker-generator.png";
 import securitySmellsDetector from "../../public/security_smells_detector.png";
+import samsung from "../../public/samsung.png";
+import queues from "../../public/queues.png";
+import amazon from "../../public/amazon.png";
+import itential from "../../public/itential.png";
 
 export const links = [
     {
@@ -36,28 +37,48 @@ export const links = [
 
 export const experiencesData = [
     {
-        title: "Graduated bootcamp",
-        location: "Miami, FL",
-        description:
-            "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-        icon: React.createElement(LuGraduationCap),
-        date: "2019"
+        title: "Software Engineer Intern",
+        company: "Samsung Electronics",
+        image: samsung,
+        points: [
+            "Implemented vRAN automation team’s CI/CD dashboard that can automatically build, deploy, and release the team organization’s Docker images to reduce more than 300 engineers’ manual job.",
+            "Add new feature that manage Docker Images’ deployments, and releases histories to help Vice President’s job.",
+            "Maintained the dashboard Frontend asynchronous data fetching gracefully with readable code and add new feature that shows deployment status in real-time with using React.js, TypeScript, React-Query, Python, Flask, WebSocket, MongoDB, Jenkins, Docker, Chakra-UI, SCSS, Figma, Perforce(P4)."
+        ],
+        date: "2023"
     },
     {
-        title: "Front-End Developer",
-        location: "Orlando, FL",
-        description:
-            "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-        icon: React.createElement(CgWorkAlt),
-        date: "2019 - 2021"
+        title: "Software Engineer (Part-time)",
+        company: "Queues",
+        image: queues,
+        points: [
+            "Scaled the Dashboard’s universities information in GA area by 50% with React.js, JavaScript, and Redux.",
+            "Edited API router to pull more important data so can reduce 50% of API request with Python, Flask, AWS EC2, MongoDB.",
+            "Minimized the delay in the Dashboard User Interfaces which was caused by asynchronous data fetching and fixed this issue with editing the Frontend component architecture."
+        ],
+        date: "2022 - 2023"
     },
     {
-        title: "Full-Stack Developer",
-        location: "Houston, TX",
-        description:
-            "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-        icon: React.createElement(FaReact),
-        date: "2021 - present"
+        title: "Software Quality Assurance Engineer Intern",
+        company: "Amazon",
+        image: amazon,
+        points: [
+            "Wrote an Amazon Alexa’s music request service’s test plan that is requested 92k per month in the US.",
+            "Re-wrote Amazon Music Voice Experience team’s automation testing scripts and improved automation tests’ passing rate up to 90% with Java, AWS EC2.",
+            "Improved my Testing Plan skillsets with thinking highly specific and infrequent test scenarios, and reviewing other QAE’s test plan helped me approach the task in diverse perspective."
+        ],
+        date: "2022"
+    },
+    {
+        title: "Software Engineer Intern",
+        company: "Itential",
+        image: itential,
+        points: [
+            "Reduced 15% of employee’s manual work by creating an internal tool for the Itential Automation Platform to retrieve information from the platform and automatically updating the Confluence page with React.js, JavaScript, Node.js, and MongoDB.",
+            "Created a script that reduced 30% of employee’s manual work which converts a specific type of network device native configuration to other types with Python.",
+            "Minimized the latencies for the internal tool, addressed by modifying the database schema and adjusting the Frontend architecture."
+        ],
+        date: "2022"
     }
 ] as const;
 
